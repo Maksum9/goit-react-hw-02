ab2267c46a31f26b2ac3ceea0c735d57546064ba
Maksym <krukm187@gmail.com>
2024-02-20 19:04:18 +0200

    Made folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
 
+const Modal = () => {
+  useEffect(() => {
+    setInterval(() => {
+      console.log(`Interval - ${Date.now()}`);
+    }, 2000);
+  }, []);
 
-function App() {
+  return <div>Modal</div>;
+};
 
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? "Close" : "Open"}
+      </button>
+      {isOpen && <Modal />}
+    </div>
+  );
+};
 
-export default App
+
+export default App;
